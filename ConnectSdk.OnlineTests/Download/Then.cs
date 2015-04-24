88459fd340f8346ae1aa8b53eee69f2fdbe67b25
skenzell <skenzell@gettyimages.com>
2015-04-24 14:20:51 -0700

    Remove download verification steps in binding.

--- ConnectSdk.OnlineTests/Download/Then.cs
+++ ConnectSdk.OnlineTests/Download/Then.cs
@@ -27,15 +27,6 @@ namespace GettyImages.Connect.Tests.Download
             Assert.IsTrue(uri.Contains(fileType), "wrong file_type is returned.");
         }
 
-        [Then(@"the url will return the largest image")]
-        public void ThenTheUrlWillReturnTheLargestImage()
-        {
-            var result = ScenarioContext.Current.Get<dynamic>("result");
-            Uri downloadUri;
-            Assert.IsTrue(Uri.TryCreate(result.uri.ToString(), UriKind.Absolute, out downloadUri),
-                string.Format("String returned was not a valid URI: {0}", result.uri.ToString()));
-        }
-
         /// <summary>
         /// </summary>
         [Then(@"the url for the image is returned")]
@@ -49,17 +40,6 @@ namespace GettyImages.Connect.Tests.Download
             Uri downloadUri;
             Assert.IsTrue(Uri.TryCreate(task.Result.uri.ToString(), UriKind.Absolute, out downloadUri),
                 string.Format("String returned was not a valid URI: {0}", task.Result.uri.ToString()));
-        }
-
-        [Then(@"the url has the correct height")]
-        public void ThenTheUrlHasTheCorrectHeight()
-        {
-            var result = ScenarioContext.Current.Get<dynamic>("result");
-            Uri downloadUri;
-            Assert.IsTrue(Uri.TryCreate(result.uri.ToString(), UriKind.Absolute, out downloadUri),
-                string.Format("String returned was not a valid URI: {0}", result.uri.ToString()));
-            string uri = result.uri;
-            Assert.IsTrue(uri.Contains("&b=RA=="), "wrong size is returned.");
         }
     }
 }
