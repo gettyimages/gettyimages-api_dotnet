a6bcd40a67952fbf66ecf7737a055f01517c9db3
mpitman <mark.pitman@gettyimages.com>
2015-04-29 13:05:57 -0700

    updating build script and nuspec for new package

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 GettyImages.Api\nuget.exe restore GettyImages.Api.sln
 copy .\specflow.exe.config packages\SpecFlow.1.9.0\tools\
 .\packages\SpecFlow.1.9.0\tools\specflow.exe generateall GettyImages.Api.OnlineTests\GettyImages.Api.OnlineTests.csproj
-msbuild.exe GettyImages.Api.sln /t:Rebuild /p:Configuration=Debug
+msbuild.exe GettyImages.Api.sln /t:Rebuild /p:Configuration=Release
 GettyImages.Api\nuget.exe pack GettyImages.Api\GettyImages.Api.csproj
