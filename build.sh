35155d3d967b4dbe6b2cac43ef4f624d1aec2a94
mpitman <mark.pitman@gettyimages.com>
2015-05-04 16:25:38 -0700

    fixed package restore

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
-ConnectSDK/nuget.exe restore ConnectSdk.sln
 cp ./specflow.exe.config packages/SpecFlow.1.9.0/tools/
-./packages/SpecFlow.1.9.0/tools/specflow.exe generateall ConnectSdk.OnlineTests/ConnectSdk.OnlineTests.csproj
-msbuild.exe ConnectSDK.sln /t:Rebuild /p:Configuration=Debug
-ConnectSDK/nuget.exe pack ConnectSDK/ConnectSdk.csproj
+./packages/SpecFlow.1.9.0/tools/specflow.exe generateall GettyImages.Api.OnlineTests/GettyImages.Api.OnlineTests.csproj
+msbuild.exe GettyImages.Api.sln /t:Rebuild /p:Configuration=Release
+.nuget/nuget.exe pack GettyImages.Api/GettyImages.Api.csproj -Prop Configuration=Release
