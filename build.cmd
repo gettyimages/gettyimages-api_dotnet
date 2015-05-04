d51580d2c0e7f6791a83ac7b73cadab54529e591
mpitman <mark.pitman@gettyimages.com>
2015-05-04 16:59:00 -0700

    fixed specflow codegen

--- build.cmd
+++ build.cmd
@@ -1,4 +1,2 @@
-copy .\specflow.exe.config packages\SpecFlow.1.9.0\tools\
-.\packages\SpecFlow.1.9.0\tools\specflow.exe generateall GettyImages.Api.OnlineTests\GettyImages.Api.OnlineTests.csproj
 msbuild.exe GettyImages.Api.sln /t:Rebuild /p:Configuration=Release
 .nuget\nuget.exe pack GettyImages.Api\GettyImages.Api.csproj -Prop Configuration=Release
