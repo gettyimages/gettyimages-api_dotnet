d32be54a4a232653fa49344f3c303966186bb4e7
mpitman <mark.pitman@gettyimages.com>
2016-01-06 10:37:54 -0800

    Bump version number to 2.3

--- GettyImages.Api/Properties/AssemblyInfo.cs
+++ GettyImages.Api/Properties/AssemblyInfo.cs
@@ -25,7 +25,7 @@ using System.Runtime.CompilerServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.2.0.0")]
-[assembly: AssemblyFileVersion("2.2.0.0")]
-[assembly: AssemblyInformationalVersion("2.2.0")]
+[assembly: AssemblyVersion("2.3.0.0")]
+[assembly: AssemblyFileVersion("2.3.0.0")]
+[assembly: AssemblyInformationalVersion("2.3.0")]
 [assembly: InternalsVisibleTo("UnitTests")]
