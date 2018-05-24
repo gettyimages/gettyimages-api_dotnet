619651f524e7b60f47711bbf7b098a8a59203f26
Stephanie Sterling <Stephanie.Sterling@gettyimages.com>
2018-05-24 08:31:41 -0700

    Add orders endpoint functionality and unit test

---
+++ UnitTests/Orders/OrdersTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using GettyImages.Api;
+using Xunit;
+
+namespace UnitTests.Orders
+{
+    public class OrdersTests
+    {
+        [Fact]
+        public void OrdersBasic()
+        {
+            var testHandler = TestUtil.CreateTestHandler();
+
+            var response = ApiClient.GetApiClientWithClientCredentials("apiKey", "apiSecret", testHandler)
+                .Orders().WithId(1234).ExecuteAsync().Result;
+
+            testHandler.Request.RequestUri.AbsoluteUri.Should().Contain("orders/1234");
+        }
+    }
+}
