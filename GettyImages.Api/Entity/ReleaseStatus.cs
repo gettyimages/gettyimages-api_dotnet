92a6a0de92a4887f8fa94058ef412693026967c3
Zelalem  Wubale <zelalem.wubale@gettyimages.com>
2018-05-25 10:16:34 -0700

    add release_status filter for video search endpoints

---
+++ GettyImages.Api/Entity/ReleaseStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace GettyImages.Api.Entity
+{
+    public enum ReleaseStatus
+    {
+        None = 0,
+        [Description("release_not_important")] ReleaseNotImportant = 1,
+        [Description("fully_released")] FullyReleased = 2,
+        [Description("partially_released")] PartiallRreleased = 3
+    }
+}
