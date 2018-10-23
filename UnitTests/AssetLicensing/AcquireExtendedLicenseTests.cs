67de1c9568d023cbd55514cd392eee799d893393
Zelalem  Wubale <zelalem.wubale@gettyimages.com>
2018-10-23 13:41:28 -0700

    add unit test for extended license SDK

---
+++ UnitTests/AssetLicensing/AcquireExtendedLicenseTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using GettyImages.Api;
+using Xunit;
+
+namespace UnitTests.AssetLicensing
+{
+    public class AcquireExtendedLicenseTests
+    {
+        [Fact]
+        public void AcquireExtendedLicenseBasic()
+        {
+            var testHandler = TestUtil.CreateTestHandler();
+            var requestBody = @"{
+                                        ['Multiseat', 'Indemnification'], 
+                                        'UseTeamCredits': true
+                                    }";
+            var assetId = "123";
+            var response = ApiClient.GetApiClientWithResourceOwnerCredentials("apiKey", "apiSecret", "userName", "userPassword",  testHandler)
+                .AcquireExtendedLicense()
+                .WithAssetId(assetId)
+                .WithExtendedLicenses(requestBody)
+                .ExecuteAsync()
+                .Result;
+
+            testHandler.Request.RequestUri.AbsoluteUri.Should().Contain(string.Format("/asset-licensing/{0}", assetId));
+            testHandler.Request.Content.ReadAsStringAsync().Result.Should().Contain(requestBody);
+        }
+    }
+}
