d5ffa92bf78aa642e0010da9632ec6f6012a06e9
Justin Greywolf <justin.greywolf@gettyimages.com>
2019-10-23 07:29:52 -0700

    Added objects/methods for new download history endpoints

---
+++ GettyImages.Api/Images/ImageDownloadHistory.cs
@@ -0,0 +1,48 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GettyImages.Api.Images
+{
+    public class ImageDownloadHistory : ApiRequest
+    {
+        private const string ImageDownloadHistoryPath = "/images/{0}/downloadhistory";
+        private string _imageId;
+
+        private ImageDownloadHistory(Credentials credentials, string baseUrl, DelegatingHandler customHandler) : base(customHandler)
+        {
+            Credentials = credentials;
+            BaseUrl = baseUrl;
+        }
+
+        internal static ImageDownloadHistory GetInstance(Credentials credentials, string baseUrl, DelegatingHandler customHandler)
+        {
+            return new ImageDownloadHistory(credentials, baseUrl, customHandler);
+        }
+
+        public override async Task<dynamic> ExecuteAsync()
+        {
+            Method = "GET";
+            Path = Path = string.Format(ImageDownloadHistoryPath, _imageId);
+
+            return await base.ExecuteAsync();
+        }
+
+        public ImageDownloadHistory WithAcceptLanguage(string value)
+        {
+            AddHeaderParameter(Constants.AcceptLanguage, value);
+            return this;
+        }
+
+        public ImageDownloadHistory WithId(string val)
+        {
+            _imageId = val;
+            return this;
+        }
+
+        public ImageDownloadHistory WithCompanyDownloads(bool value = true)
+        {
+            AddQueryParameter(Constants.CompanyDownloadsKey, value);
+            return this;
+        }
+    }
+}
---
+++ UnitTests/Images/ImagesDownloadHistoryTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using GettyImages.Api;
+using Xunit;
+
+namespace UnitTests.Images
+{
+    public class ImagesDownloadHistoryTests
+    {
+        [Fact]
+        public void ImageDownloadHistoryWithCompanyDownloads()
+        {
+            var testHandler = TestUtil.CreateTestHandler();
+
+            var response = ApiClient.GetApiClientWithClientCredentials("apiKey", "apiSecret", testHandler)
+               .ImageDownloadHistory().WithId("882449540").WithCompanyDownloads().ExecuteAsync().Result;
+
+            testHandler.Request.RequestUri.AbsoluteUri.Should().Contain("images/882449540/downloadhistory");
+            testHandler.Request.RequestUri.AbsoluteUri.Should().Contain("company_downloads=True");
+        }
+
+        [Fact]
+        public void DownloadHistoryForImage()
+        {
+            var testHandler = TestUtil.CreateTestHandler();
+
+            var response = ApiClient.GetApiClientWithClientCredentials("apiKey", "apiSecret", testHandler)
+               .ImageDownloadHistory().WithId("882449540").ExecuteAsync().Result;
+
+            testHandler.Request.RequestUri.AbsoluteUri.Should().Contain("images/882449540/downloadhistory");
+        }
+    }
+}
