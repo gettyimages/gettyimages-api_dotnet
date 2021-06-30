6d4f56e039d4550c9e69e13d7853dc6587a91fcd
Chris Simmons <chris.simmons@gettyimages.com>
2021-06-30 16:27:18 -0700

    Use 100ms * retryAttempt for retry policy

--- GettyImages.Api/PollyHttpClientExtensions.cs
+++ GettyImages.Api/PollyHttpClientExtensions.cs
@@ -18,7 +18,7 @@ namespace GettyImages.Api
                     .HandleTransientHttpError()
                     .OrResult(responseMessage => (int) responseMessage.StatusCode == 429)
                     .WaitAndRetryAsync(3, retryAttempt =>
-                        TimeSpan.FromSeconds(retryAttempt)
+                        TimeSpan.FromMilliseconds(retryAttempt * 100)
                     );
         }
 
