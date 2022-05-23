5fe8917eaa1e562e43b27174e215beeeabb79b81
Stephanie Sterling <Stephanie.Sterling@gettyimages.com>
2022-05-23 14:58:58 -0700

    Remove gif and png file types

--- GettyImages.Api/Models/FileType.cs
+++ GettyImages.Api/Models/FileType.cs
@@ -7,7 +7,5 @@ public enum FileType
 {
     None = 0,
     [Description("eps")] Eps = 1,
-    [Description("gif")] Gif = 2,
-    [Description("jpg")] Jpg = 4,
-    [Description("png")] Png = 8
+    [Description("jpg")] Jpg = 2
 }
