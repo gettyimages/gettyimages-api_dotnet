1fc42979c3556f6fabc8a8756c187783612e57ee
Guoyu Wang <gwang@microsoft.com>
2022-07-18 15:57:47 -0700

    Add strong name signing

--- GettyImages.Api/Handlers/HeadersHandler.cs
+++ GettyImages.Api/Handlers/HeadersHandler.cs
@@ -4,7 +4,12 @@ using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 
-[assembly: InternalsVisibleTo("UnitTests")]
+[assembly: InternalsVisibleTo("UnitTests,PublicKey="+
+"0024000004800000940000000602000000240000525341310004000001000100a182a8a01ddd98"+
+"2ea41be8b6181820cad40060e97fed5cba8c7fd2532600913ef061fbf59c95adf05e6e7ba22429"+
+"70d8da87b876e491beacaf535ee6efc348b0bded515a96ccaa1bd2612652f874371003b5d4cb7d"+
+"345a33e4ca99b07d94cd339921948548752c17e8c49d9baf8304e4bb735de95fb0a4921b14388b"+
+"040472bc")]
 
 namespace GettyImages.Api.Handlers;
 
