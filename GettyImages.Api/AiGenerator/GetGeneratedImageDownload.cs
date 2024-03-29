5df4086d122eeb29f0c1f1220143a1848fb54951
Chris Simmons <chris.simmons@gettyimages.com>
2024-03-29 16:02:05 -0700

    Split to classes

--- GettyImages.Api/AiGenerator/GetGeneratedImageDownload.cs
+++ GettyImages.Api/AiGenerator/GetGeneratedImageDownload.cs
@@ -5,29 +5,6 @@ using GettyImages.Api.Models;
 
 namespace GettyImages.Api.AiGenerator;
 
-public class GetDownloadSizes:ApiRequest<GeneratedDownloadSizesResponse>
-{
-    private GetDownloadSizes(Credentials credentials, string baseUrl, DelegatingHandler customHandler) : base(
-        customHandler)
-    {
-        Credentials = credentials;
-        Method = "GET";
-        BaseUrl = baseUrl;
-    }
-
-    internal static GetDownloadSizes GetInstance(Credentials credentials, string baseUrl,
-        DelegatingHandler customHandler)
-    {
-        return new GetDownloadSizes(credentials, baseUrl, customHandler);
-    }
-
-    public GetDownloadSizes With(string generationRequestId, int index)
-    {
-        Path = $"/ai/image-generations/{generationRequestId}/images/{index}/download-sizes";
-        return this;
-    }
-}
-
 // TODO - Naming?
 public class GetGeneratedImageDownload : PolledPathApiRequest<DownloadGeneratedImageReadyResponse>
 {
